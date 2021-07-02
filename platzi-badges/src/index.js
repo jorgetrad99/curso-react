8d7db1a87392366e0f166266dea5fc301309da5e
Jorge Trad <jetradl@gmail.com>
2021-07-02 13:23:41 -0500

    Our first page

--- platzi-badges/src/index.js
+++ platzi-badges/src/index.js
@@ -3,17 +3,11 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 
 import "./global.css";
-import Badge from "./components/Badge";
+import BadgeNew from "./pages/BadgeNew";
 
 const container = document.getElementById("app");
 
-ReactDOM.render(
-  <Badge
-    firstName="Lily"
-    lastName="Kaufman"
-    jobTitle="Frontend Engineer"
-    twitter="sparragus"
-    avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
-  />,
-  container
-);
+/* const element = <BadgeNew />; */
+
+/* ReactDOM.render(<BadgeNew />, container); */
+ReactDOM.render(<BadgeNew />, container);
