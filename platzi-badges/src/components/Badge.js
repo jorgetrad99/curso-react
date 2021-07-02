2aebee98c39fb4485d35171fc6e6f5bf9150da64
Jorge Trad <jetradl@gmail.com>
2021-07-02 11:54:14 -0500

    Props: arguments to our components

--- platzi-badges/src/components/Badge.js
+++ platzi-badges/src/components/Badge.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import './styles/Badge.css';
-import confLogo from '../images/badge-header.svg';
+import "./styles/Badge.css";
+import confLogo from "../images/badge-header.svg";
 
 class Badge extends React.Component {
   render() {
+    const { firstName, lastName, jobTitle, twitter, avatarUrl } = this.props;
     return (
       <div className="Badge">
         <div className="Badge__header">
@@ -12,19 +13,15 @@ class Badge extends React.Component {
         </div>
 
         <div className="Badge__section-name">
-          <img
-            className="Badge__avatar"
-            src="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
-            alt="Avatar"
-          />
+          <img className="Badge__avatar" src={avatarUrl} alt="Avatar" />
           <h1>
-            Richard <br /> Kaufman
+            {firstName} <br /> {lastName}
           </h1>
         </div>
 
         <div className="Badge__section-info">
-          <h3>Frontend Engineer</h3>
-          <div>@sparragus</div>
+          <h3>{jobTitle}</h3>
+          <div>@{twitter}</div>
         </div>
 
         <div className="Badge__footer">#platziconf</div>
